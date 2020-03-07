d69db7b8d2d562d8745217927650be0d775f712e
Shaina <shaina.sierra@gmail.com>
2020-03-07 11:57:09 -0800

    Initial setup of WatchConnectivity

--- WatchConnectivityTest/ViewController.swift
+++ WatchConnectivityTest/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WatchConnectivity
+class ViewController: UIViewController, WCSessionDelegate {
+    
+    var lastMessage: CFAbsoluteTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if (WCSession.isSupported()) {
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+
+    }
+
+    func sessionDidBecomeInactive(_ session: WCSession) {
+
+    }
+
+    func sessionDidDeactivate(_ session: WCSession) {
+
     }
 
 
