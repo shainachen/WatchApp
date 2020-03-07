118639056ec6ae1e613f4ca312a6f89da3e99407
Shaina <shaina.sierra@gmail.com>
2020-03-07 11:53:40 -0800

    Initial commit

---
+++ WatchConnectivityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchConnectivityTest
+//
+//  Created by shaina on 3/7/20.
+//  Copyright © 2020 shaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
